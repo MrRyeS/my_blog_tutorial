9b2bc2e4a8e0d22d17e7c6d6877a520e5a1c4916
xieqianw <xieqianw@xieqianw-mbp-36137.sv.splunk.com>
2018-08-02 10:16:08 +0800

    tutorial finished

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 from article import views as article_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', article_views.home, name='home'),
-    url(r'^(?P<my_args>\d+)/$', article_views.detail, name='detail'),
-    url(r'^test/$', article_views.test),
+    url(r'^(?P<id>\d+)/$', article_views.detail, name='detail'),
+    url(r'^tag/(?P<tag>\w+)/$', article_views.search_tag, name = 'search_tag'),
+    url(r'^archives/$', article_views.archives, name = 'archives'),
+    url(r'^aboutme/$', article_views.about_me, name = 'about_me'),
+    url(r'^search/$', article_views.blog_search, name = 'search'),
+
 ]
